88b3ca65521b953903f6ea672732304d207b950b
Szymon Jedras <szymon.jedras.wr@gmail.com>
2022-05-06 02:01:21 +0200

    typo

--- test2.sh
+++ test2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ ( 1 == "$1" ) || ( 2 == "$1" ) || ( 3 == "$1" ) || ( 4 == "$1" ) ]]; then
-  ./test/test2/congfigure.sh $1
+  ./test/test2/configure.sh $1
   ./router < "./test/test2/config$1"
 fi
