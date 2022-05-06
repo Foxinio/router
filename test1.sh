ee64d1a4f01f7d5d18e45bb1ec5f5e758c227626
Szymon Jedras <szymon.jedras.wr@gmail.com>
2022-05-06 03:09:45 +0200

    fixed multiple configuring

--- test1.sh
+++ test1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ ( 1 == "$1" ) || ( 2 == "$1" ) || ( 3 == "$1" ) || ( 4 == "$1" ) ]]; then
-  ./test/test1/congfigure.sh $1
+  [[ ! -e ./test/test1/configured ]] && ./test/test1/configure.sh $1
   ./router < "./test/test1/config$1"
 fi
--- test2.sh
+++ test2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ ( 1 == "$1" ) || ( 2 == "$1" ) || ( 3 == "$1" ) || ( 4 == "$1" ) ]]; then
-  ./test/test2/configure.sh $1
+  [[ ! -e ./test/test2/configured ]] && ./test/test2/configure.sh $1
+  touch ./test/test2/configured
   ./router < "./test/test2/config$1" 2> debug.out
 fi
